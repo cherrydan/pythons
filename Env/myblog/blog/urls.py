24c0730dcaab20391b6eb00bf8705526ea52412b
Danil <cherrydan.1978@gmail.com>
2020-04-27 12:11:37 +0300

    Сделал всё про детализацию поста

--- Env/myblog/blog/urls.py
+++ Env/myblog/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list, name='post_list')
+    path('',views.post_list, name='post_list'),
+    #шаблон заголовка url слово post, косая черта, primary key и еще косая черта
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- Env/myblog/blog/views.py
+++ Env/myblog/blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from . models import Post
 
 # Create your views here.
+
+'''
+выводит список всех постов блога, сортирует их по дате публикации
+'''
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+
+'''
+выводит единичный пост блога, добавляя primary key в url
+'''
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html', {'post':post})
