03a8b491d75030533b08c88bb7c4d7102a0b100a
Danil <cherrydan.1978@gmail.com>
2020-05-05 20:09:01 +0300

    Немножко помучил исходник pylint-ом

--- Parsing/parser_tel.py
+++ Parsing/parser_tel.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#module for http requests
-import requests
-
-#module for scrapping
-from bs4 import BeautifulSoup
 
 #module for make csv file
 import csv
@@ -19,6 +14,13 @@ from datetime import datetime
 #module for sleep function
 import time
 
+#importing Beautiful Soup library
+from bs4 import BeautifulSoup
+
+#importing requests library
+import requests
+
+
 #importing progress-bar drawing library
 from progress.bar import IncrementalBar
 
@@ -29,74 +31,70 @@ from selenium import webdriver
 URL = 'https://auto.ria.com/newauto/marka-lexus/'
 
 #short url of cars
-sURL = 'https://auto.ria.com'
+S_URL = 'https://auto.ria.com'
 
 # anti-blocking protection
-HEADERS = {'user-agent': 
-'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-'accept': '*/*'
-}
+HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+           'accept': '*/*'}
 
 #filename for csv-file
 FILE = 'cars.csv'
 
 # function gets html data via requests library
-def get_html(url, params = None):
-    r = requests.get(url, headers = HEADERS, params = params)
-    return r
+def get_html(url, params=None):
+    request = requests.get(url, headers=HEADERS, params=params)
+    return request
 
 
 # function gets number of pages in our query-url
 def get_pages_count(html):
-    soup = BeautifulSoup(html,'html.parser')
-    pagination = soup.find_all('span',class_='mhide')
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='mhide')
     if pagination:
         return int(pagination[-1].get_text()) # gets last element of pagination array
-    else:
-        return 1
 
-#opens Firefox via geckodriver with link
+    return 1
+
+###SELENIUM
+#opens Chrome via chromedriver  with link
+#finds 'show phone number' button
+#emulates button click
+#finds element with phone number and GET its as text
+#returns phone number
 def get_telephone_number(link):
-    try:
-        driver = webdriver.Chrome()
-        driver.get(link)
-        button = driver.find_element_by_xpath('//span[@class="show-phone-btn dotted"]')
-        button.click()
-        time.sleep(1)
-        # driver.save_screenshot('tel_number.png')
-        phone_number = button.find_element_by_xpath('//a[@class="phone unlink bold proven"]').text
-        return phone_number
-    except Exception as err:
-        print(err)
+    driver = webdriver.Chrome()
+    driver.get(link)
+    button = driver.find_element_by_xpath('//span[@class="show-phone-btn dotted"]')
+    button.click()
+    time.sleep(1)
+    # driver.save_screenshot('tel_number.png')
+    phone_number = button.find_element_by_xpath('//a[@class="phone unlink bold proven"]').text
+    return phone_number
 
 
 
-
+### BEAUTIFUL SOUP
 # function fills 'cars' dictionary with data, parsed by Beautiful Soup library
+# and with get_telephone_number function
 def get_content(html):
-    soup = BeautifulSoup(html,'html.parser')
-    items = soup.find_all('div',class_='proposition_area')
-    cars = []
-    
-    bar = IncrementalBar('',max=len(items))
-    
-    counter = 0
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='proposition_area')
+    cars = [] 
+    prog_bar = IncrementalBar('', max=len(items))
     for item in items:
-        #print(item)
-        uah_price = item.find('span',class_='grey size13')
+        uah_price = item.find('span', class_='grey size13')
         if uah_price:
             uah_price = uah_price.get_text(strip=True)
         else:
             uah_price = 'Цена в гривнах не определена'
 
         title = item.find('strong').get_text(strip=True) #strip = обрезает лишние пробелы
-        usd_price = item.find('span',class_='green bold size18').get_text(strip=True) 
-        link = sURL + item.find('a').get('href')
+        usd_price = item.find('span', class_='green bold size18').get_text(strip=True) 
+        link = S_URL + item.find('a').get('href')
         fuel = item.find('span', class_='size13').get_text()
         city = item.find('svg', class_='svg svg-i16_pin').find_next('strong').get_text(strip=True)
-   
         tel_number = get_telephone_number(link)
-        if (tel_number):
+        if tel_number:
             cars.append({
                 'title': title,
                 'usd_price': usd_price,
@@ -116,34 +114,38 @@ def get_content(html):
                 'city': city,    
                 'telephone': 'Телефон не указан'})
         time.sleep(0.3)
-        bar.next()
-
-    bar.finish()    
+        prog_bar.next()
+    prog_bar.finish()    
     return cars
 
 # function saves 'items' to 'path' in csv file        
 def save_file(items, path):
-    with open(path,'w',newline='') as file:
-        writer = csv.writer(file,delimiter=';')
-        writer.writerow(['Марка','Цена в USD','Цена в UAH','Ссылка','Двигатель','Город','Телефон'])
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Марка', 'Цена в USD', 'Цена в UAH', 'Ссылка', 
+                         'Двигатель', 'Город', 'Телефон'])
 
         for item in items:
-            writer.writerow([item['title'],item['usd_price'],item['uah_price'],item['link'],item['fuel'],item['city'],item['telephone'],])
-
-
-
-
+            writer.writerow([item['title'], item['usd_price'], item['uah_price'], 
+                             item['link'], item['fuel'], item['city'], item['telephone'],])
 
 # function parse() makes test parsing
 def parse():
-    URL = input('Введите URL (наподобие https://auto.ria.com/newauto/marka-acura)  > ')
-    URL = URL.strip()
+    url = input('Введите URL (наподобие https://auto.ria.com/newauto/marka-acura)  > ')
+    url = url.strip()
     start_time = datetime.now()
-    html = get_html(URL)
+    html = get_html(url)
     cars = []
     if html.status_code == 200: 
         cars.extend(get_content(html.text))
-        save_file(cars,'cars.csv')
+        save_file(cars, 'cars.csv')
+        print(f'Получено {len(cars)} автомобилей.')
+        elapsed_time = datetime.now() - start_time
+        elapsed_time = elapsed_time.total_seconds()
+        hours = int(elapsed_time // 3600)
+        minutes = int((elapsed_time % 3600) // 60)
+        seconds = int(elapsed_time % 60)
+        print(f'Затрачено времени: {hours} ч {minutes} м {seconds} с ')
     else:
         print('Error')
 
