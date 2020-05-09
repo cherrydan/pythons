11dbc9e32ca6f8adad9f0f75189887e3c23fa10e
Danil <cherrydan.1978@gmail.com>
2020-05-09 14:43:13 +0300

    Залинтовал то, что должно быть в многопоточном режиме. Никак не доберусь

--- MultiprocDemo/mparser.py
+++ MultiprocDemo/mparser.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-
 Parser for site auto.ria.com, new auto
 multiprocessing version
 '''
@@ -28,7 +27,7 @@ def get_all_page_urls(url, max_page):
     """
     urls = []
     for page in range(max_page):
-    	urls.append(url + '?page='+str(page+1))
+        urls.append(url + '?page='+str(page+1))
     return urls
 
 def get_pages_count(html):
@@ -64,29 +63,28 @@ def get_telephone_number(link):
     return phone_number
 
 def get_page_data(html):
-	"""
-	Функция получает html-данные со страницы
-	returns все нужные нам данные
-	"""
-	soup = BeautifulSoup(html, 'html.parser')
-	items = soup.find_all('div', class_='proposition_area')
-	cars = []
-	prog_bar = IncrementalBar('', max=len(items))
-	for item in items:
-		uah_price = item.find('span', class_='grey size13')
-		if uah_price:
-			uah_price = uah_price.get_text(strip=True)
-		else:
-			uah_price = 'Цена в гривнах не определена'
-
-		title = item.find('strong').get_text(strip=True) #strip = обрезает лишние пробелы
-		usd_price = item.find('span', class_='green bold size18').get_text(strip=True)
-		link = DOMAIN + item.find('a').get('href')
-		fuel = item.find('span', class_='size13').get_text()
-		city = item.find('svg', class_='svg svg-i16_pin').find_next('strong').get_text(strip=True)
-		tel_number = get_telephone_number(link) 
-		if tel_number:
-			cars.append({
+    """
+    Функция получает html-данные со страницы
+    returns все нужные нам данные
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='proposition_area')
+    cars = []
+    prog_bar = IncrementalBar('', max=len(items))
+    for item in items:
+        uah_price = item.find('span', class_='grey size13')
+        if uah_price:
+            uah_price = uah_price.get_text(strip=True)
+        else:
+            uah_price = 'Цена в гривнах не определена'
+        title = item.find('strong').get_text(strip=True) #strip = обрезает лишние пробелы
+        usd_price = item.find('span', class_='green bold size18').get_text(strip=True)
+        link = DOMAIN + item.find('a').get('href')
+        fuel = item.find('span', class_='size13').get_text()
+        city = item.find('svg', class_='svg svg-i16_pin').find_next('strong').get_text(strip=True)
+        tel_number = get_telephone_number(link)
+        if tel_number:
+            cars.append({
                 'title': title,
                 'usd_price': usd_price,
                 'uah_price': uah_price,
@@ -94,8 +92,8 @@ def get_page_data(html):
                 'fuel': fuel,
                 'city': city,
                 'telephone': tel_number})
-		else:
-			cars.append({
+        else:
+            cars.append({
                 'title': title,
                 'usd_price': usd_price,
                 'uah_price': uah_price,
@@ -103,51 +101,51 @@ def get_page_data(html):
                 'fuel': fuel,
                 'city': city,
                 'telephone': 'Телефон доступен в рабочие часы'})
-		prog_bar.next()
-	prog_bar.finish()
-	return cars
+        prog_bar.next()
+    prog_bar.finish()
+    return cars
 
 def write_csv(items):
-	"""
-	Функция принимает словарь с данными, пишет в дефолтный файл cars.csv
-	"""
-	with open('cars.csv', 'w', newline='') as file:
-		writer = csv.writer(file, delimiter=';')
-		writer.writerow(['Марка', 'Цена в USD', 'Цена в UAH', 'Ссылка',
+    """
+    Функция принимает словарь с данными, пишет в дефолтный файл cars.csv
+    """
+    with open('cars.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Марка', 'Цена в USD', 'Цена в UAH', 'Ссылка',
                          'Двигатель', 'Город', 'Телефон'])
-		for item in items:
-			writer.writerow([item['title'], item['usd_price'], item['uah_price'],
-                                         item['link'], item['fuel'], item['city'], item['telephone'],])
+        for item in items:
+            writer.writerow([item['title'], item['usd_price'], item['uah_price'],
+                             item['link'], item['fuel'], item['city'], item['telephone'],])
 
 def main():
-	"""
-	Main-функция точка входа в программу
-	"""
-	url = input('Введите url (наподобие https://auto.ria.com/newauto/marka-jeep/) >')
-	url = url.strip()
-	start_time = datetime.now()
-	all_urls = []
-	data = []
-	pages_count = get_pages_count(get_html(url))
-	all_urls = get_all_page_urls(url, pages_count)
-	#вывести что то типа "Парсинг страницы X из Y"
-	#сделать замеры времени выполнения
-	counter = 1
-	for url in all_urls:
-		html = get_html(url)
-		print(f'Обработка страницы {counter} из {pages_count}')
-		counter+=1
-		data.extend(get_page_data(html))
-	print(f'Получено данных о {len(data)} автомобилей')
-	write_csv(data)
-	elapsed_time = datetime.now() - start_time
-	elapsed_time = elapsed_time.total_seconds()
-	hours = int(elapsed_time // 3600)
-	minutes = int((elapsed_time % 3600) // 60)
-	seconds = int(elapsed_time % 60)
-	print(f'Затрачено времени: {hours} ч {minutes} м {seconds} с ')	
+    """
+    Main-функция точка входа в программу
+    """
+    url = input('Введите url (наподобие https://auto.ria.com/newauto/marka-jeep/) >')
+    url = url.strip()
+    start_time = datetime.now()
+    all_urls = []
+    data = []
+    pages_count = get_pages_count(get_html(url))
+    all_urls = get_all_page_urls(url, pages_count)
+    #вывести что то типа "Парсинг страницы X из Y"
+    #сделать замеры времени выполнения
+    counter = 1
+    for url in all_urls:
+        html = get_html(url)
+        print(f'Обработка страницы {counter} из {pages_count}')
+        counter += 1
+        data.extend(get_page_data(html))
+    print(f'Получено данных о {len(data)} автомобилей')
+    write_csv(data)
+    elapsed_time = datetime.now() - start_time
+    elapsed_time = elapsed_time.total_seconds()
+    hours = int(elapsed_time // 3600)
+    minutes = int((elapsed_time % 3600) // 60)
+    seconds = int(elapsed_time % 60)
+    print(f'Затрачено времени: {hours} ч {minutes} м {seconds} с ')
 #записать данные в csv
 
 
 if __name__ == '__main__':
-	main()
+    main()
