80eac891285db60d1abf4c741680010b0de352f4
Danil <cherrydan.1978@gmail.com>
2020-05-18 11:41:14 +0300

    закончил с однопоточным video_dur

--- VideoDuration/video_dur.py
+++ VideoDuration/video_dur.py
@@ -6,51 +6,76 @@ mp4.
 @author: Олег Молчанов
 """
 import os
-from subprocess import *
-#доступные нам расширения видеофайлов
-VIDEO_EXTENSIONS = ['avi','mp4','mkv']
+from subprocess import Popen, PIPE, STDOUT
+from sys import exit
+from datetime import timedelta
+# доступные нам расширения видеофайлов
+VIDEO_EXTENSIONS = ['avi', 'mp4', 'mkv']
+
 
 def get_videos():
     """
     Получаем имена видеофайлов во всех папках рекурсивно
     """
     videos = []
-    #проходка по каталогам, подкаталогам и файлам при помощи
-    #модуля os
+    # проходка по каталогам, подкаталогам и файлам при помощи
+    # модуля os
     for root, subfolders, files in os.walk(os.getcwd()):
         for FILE in files:
-            #если в файле допустимое для нас расширение видеоформата
+            # если в файле допустимое для нас расширение видеоформата
             if FILE.split('.')[-1] in VIDEO_EXTENSIONS:
-                #добавляем путь в список
+                # добавляем путь в список
                 videos.append(os.path.join(root, FILE))
             else:
-                #или покидаем функцию с сообщением
+                # или покидаем функцию с сообщением
                 return print('В папке нет допустимых видеофайлов')
     return videos
+
 
 def get_ffprobe_output(filename):
     """
     Запускаем программу ffprobe и получает её вывод
     """
-    #заменяем пробелы на экранированные пробелы, чтобы не мешать команде ffprobe
-    filename = filename.replace(' ','\ ')
-    #команда для выполнения
+    # заменяем пробелы на экранированные пробелы
+    # чтобы не мешать команде ffprobe
+    filename = filename.replace(' ', r'\ ')
+    # команда для выполнения
     cmd = 'ffprobe -i {} -show_entries format=duration -v quiet -of csv="p=0"'.format(filename)
-    #создаем подпроцесс при помощи класса popen
-    #shell=False предохраняет нашу программу от иньекций извне, но я пока хз как это сделать
-    #также перенаправляем вывод
-    popen = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
-    #получаем вывод в переменную
-    output = popen.stdout.read()
-    print(output)
+    # создаем подпроцесс при помощи класса popen
+    # shell=False предохраняет нашу программу от иньекций извне
+    # также перенаправляем вывод
+    popen = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT,
+                  universal_newlines=True)
+    # получаем данные
+    stdout, stderr = popen.communicate()
+    return int(float(stdout))
+
+
+def seconds_to_str(s):
+    """
+    Конвертирует секунды в ч.м.с
+    принимает время в секундах
+    """
+    return str(timedelta(seconds=s))
+
 
 def main():
     """
     Точка входа в программу
     """
     videos = get_videos()
-    for video in videos:
-        get_ffprobe_output(video)
+    total_duration = 0
+    if videos is not None:
+        for video in videos:
+            duration = get_ffprobe_output(video)
+            print('File: ', video.split('/')[-1], ' Duration: ',
+                  seconds_to_str(duration))
+            total_duration += duration
+        print('\nTotal duration: ', seconds_to_str(total_duration))
+    else:
+        print('Что-то пошло не так...')
+        exit(-1)
+
 
 if __name__ == '__main__':
     main()
