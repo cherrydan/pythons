803afdf09cc5aab9611452b35b689f2c70602bce
Danil <cherrydan.1978@gmail.com>
2020-05-19 19:32:45 +0300

    небольшой пример из книги Пола Берри насчёт словарей

---
+++ People/people.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# создаём словарь словарей
+people = {}
+
+people['Ford'] = {
+    'Name': 'Ford Perfect',
+    'Gender': 'Male',
+    'Occupation': 'Researcher',
+    'Home Planet': 'Bettelgeuse Seven'
+}
+
+people['Arthur'] = {
+    'Name': 'Arthur Dent',
+    'Gender': 'Male',
+    'Occupation': 'Sandwich-Maker',
+    'Home Planet': 'Earth'
+}
+
+people['Trillian'] = {
+    'Name': 'Tricia McMillan',
+    'Gender': 'Female',
+    'Occupation': 'Mathematician',
+    'Home Planet': 'Earth'
+}
+
+people['Robot'] = {
+    'Name': 'Marvin',
+    'Gender': 'Unknown',
+    'Occupation': 'Paranoid Android',
+    'Home Planet': 'Unknown'
+}
+
+print(people)
