87e315a605c9746acc705f96a9c6013689ed74f5
Danil <cherrydan.1978@gmail.com>
2020-06-22 11:51:13 +0300

    Ханойские башни на Пайтон, динамическое программирование сверху и снизу

--- Recursion/fib.py
+++ Recursion/fib.py
@@ -4,19 +4,40 @@
 Рекурсивные алгоритмы
 Числа Фибоначчи.
 (Тимофей Хирьянов)
+Динамическое программирование сверху и снизу
 """
+cache =[0]*50
+# динамическое программирование сверху
 def fib(n):
     """
-    fib(n)
+    fib(int)
     возвращает n-тое число из ряда Фибоначчи
+    Использована рекурсия с кешем
     """
     if n <= 1:
         return n
-    return fib(n-1) + fib(n-2)
+    if cache[n] == 0:
+        cache[n] = fib(n-1) + fib(n-2)
+    return cache[n]
+
+
+#динамическое программирование снизу
+def fib_dynamic(n):
+    """
+    fib_dynamic(int)
+    возвращает n-тое число Фибоначчи
+    Не использует рекурсию
+    """
+    Fib = [0] * (n + 1)
+    Fib[0] = 0
+    Fib[1] = 1
+    for i in range(2, len(Fib)):
+        Fib[i] = Fib[i - 1] + Fib[i - 2]
+    return Fib[n]
 
 def main():
-    n = int(input())
-    print('fib({}) = {}'.format(n,fib(n)))
+    for n in range(1,50):
+        print('fib({}) = {}'.format(n,fib_dynamic(n)))
 
 
 
---
+++ Trajectories/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def num_of_traje(n):
+    """
+    num_of_traje(int)
+    """
+    K = [0] * (n+1)
+    K[0] = 0
+    K[1] = 1
+    for i in range(2, len(K)):
+        K[i] = K[i-1] + K[i - 2]
+    return K[n]
+
+
+def main():
+    finish = int(input('->'))
+    print('Кузнечик имеет {} траектории от 1 до {}'.format(num_of_traje(finish), finish))
+
+if __name__ == '__main__':
+    main()
+
