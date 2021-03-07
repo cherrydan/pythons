634a727eb10b3b824b31870da7a447951648a921
Danny <cherrydan.1978@gmail.com>
2021-03-07 15:45:59 +0300

    Уроки по книжке Эрика Мэтиза

---
+++ Eric Matiz/Arrays/arr_del.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducatti']
+
+print(motorcycles)
+
+# удаление элемента с заданным индексом при помощи del
+
+del motorcycles[2]
+
+print(motorcycles)
---
+++ TkQuiz/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from tkinter import *
+root = Tk()
+
+root.title('Викторина')
+
+root.geometry('400x400')
+
+root.mainloop()
