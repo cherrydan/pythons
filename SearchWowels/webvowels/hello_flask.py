e281edb3db72434a0349953fda7d68a5a4287273
Danny <cherrydan.1978@gmail.com>
2021-03-07 15:39:10 +0300

    Welcome back to Linux! Do web application with Flask

--- SearchWowels/webvowels/hello_flask.py
+++ SearchWowels/webvowels/hello_flask.py
@@ -16,12 +16,18 @@ def hello() -> str:
 def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
+    title = 'Here is your results: '
+    results = str(vsearch.search4letters(phrase, letters))
 
-    return str(vsearch.search4letters(phrase, letters))
+    return render_template('results.html', the_phrase=phrase,
+                           the_letters=letters,
+                           the_title = title,
+                           the_results=results,)
 
 
 @app.route('/entry')
 def entry_page() -> 'html':
+
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the Web!')
 
