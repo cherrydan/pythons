6c9318a9e86752d13014005e9e77f7dd263e7ddb
Danny <cherrydan.1978@gmail.com>
2021-03-10 20:04:44 +0300

    Слизал игрушку Змейка у Хауди Хо и еще один пример из Эрика Мэтиза

---
+++ Eric Matiz/Lists/lst_sort.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# сортировка списка из слов, набранных латиницей
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print('Исходный список')
+print(cars)
+
+print('Отсортированный список')
+cars.sort()
+print(cars)
