f513bb736f222adb76ae38988b65abeb2c478352
cherrydan <cherrydan.1978.gmail.com>
2021-03-14 17:25:52 +0300

    Starts Stegano project

--- PyEnc/main.py
+++ PyEnc/main.py
@@ -3,16 +3,17 @@
 
 import os
 
+
 def crypt(dir):
     import pyAesCrypt
     print('-----------------')
     password = input('Enter password: ')
     buffer_size = 512 * 1024
-    pyAesCrypt.encryptFile(str(dir), str(dir) + '.aes', 
-            password, buffer_size)
+    pyAesCrypt.encryptFile(str(dir), str(dir) + '.aes',
+                           password, buffer_size)
     print('File ' + str(dir) + ' crypted to ' + str(dir) + '.aes')
+
 
 if __name__ == '__main__':
     dir = input('Enter filename to encrypt: ')
     crypt(dir)
-
